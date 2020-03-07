7446d559d819bbe58b2159246cc94383abd8ddf8
danilshvalov <shvalovich@gmail.com>
2020-03-07 16:48:29 +1000

    Обработчик формы авторизации - Яндекс.Лицей

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ def login():
         user = session.query(User).filter(User.email == form.email.data).first()
         if user and user.check_password(form.password.data):
             login_user(user, remember=form.remember_me.data)
-            return redirect("/")
+            return redirect("/works_log")
         return render_template('login.html',
                                message="Неправильный логин или пароль",
                                form=form)
